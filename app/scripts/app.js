5be945a78aa7a39844842fa86e0a150730ea244b
sirajrkhan <sirajrkhan@live.in>
2014-11-20 07:49:23 +0000

    updated version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+    .when('/contact',{
+        templateUrl: 'views/contact.html' ,
+        controller: 'ContactCtrl'
+    })
       .otherwise({
         redirectTo: '/'
       });
