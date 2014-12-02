d63cac669db3227fefdd4edb2401fdba818305a1
sirajrkhan <sirajrkhan@live.in>
2014-12-02 08:32:22 +0000

    updated HeaderController

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -3,12 +3,8 @@
 angular.module("emailApp")
     .controller('HeaderCtrl', function($scope, $location){
     
+    //Check which link is active and accordingly highlight the button
     $scope.isActive = function(viewLocation){
-        console.log('view loc:'+viewLocation+ ' ----- loc path:'+$location.path());
         return (viewLocation === $location.path());
-    }
-    
-    //coming up...
-    //
-    
+    }    
 });
